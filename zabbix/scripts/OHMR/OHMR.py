8b40967b4f21aaff3983bf8f1f5b957130a4363f
mihanik <mihanik2000@mail.ru>
2020-02-02 20:48:14 +0300

    / ‘zabbix/scripts/OHMR/OHMR.py’

--- zabbix/scripts/OHMR/OHMR.py
+++ zabbix/scripts/OHMR/OHMR.py
@@ -3,3 +3,30 @@
 import os
 import sys
 
+#
+# Функция проверки наличия прав администратора
+# Вход: нет
+# Выход: true - есть права администратора,
+#        false - нет прав администратора
+#
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+################################################################################
+#   Начало программы
+################################################################################
+def main(argv=None):
+   # Проверяем наличие прав админа.
+   if not is_admin ():
+      sys.exit ('Not enough permissions to run the script !!!')
+   
+   # Устанавливаем кодировку по умолчанию.
+   reload(sys)
+   sys.setdefaultencoding('utf8')
+
+
+if __name__ == "__main__":
+    sys.exit(main())
