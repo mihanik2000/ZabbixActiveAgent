4d5032b1e4e5c0d8f09d95b47940006822d6097d
mihanik <mihanik2000@mail.ru>
2020-02-13 20:58:13 +0300

    / ‘zabbix/scripts/AV_Common_Status/INSTALL_AV_Common_Status.bat’ + ‘zabbix/scripts/AV_Common_Status/nppBackup/’ + ‘zabbix/scripts/AV_Common_Status/nppBackup/INSTALL_AV_Common_Status.bat.2020-02-13_205811.bak’

--- zabbix/scripts/AV_Common_Status/INSTALL_AV_Common_Status.bat
+++ zabbix/scripts/AV_Common_Status/INSTALL_AV_Common_Status.bat
@@ -1,17 +1,50 @@
 @echo off
 
-rem Скрипт за пускается на Windows XP?
+Rem Предполагаем, что на Windows XP скрипт запускается администратором.
+Rem Для более старших систем это неверно.
+
+rem Получаем версию ОС
+ver | find "5.1."
+
+rem Windows XP ?
+If %errorlevel%==0  (
+	rem Пропускаем проверку админских прав
+	GOTO SKIPADMIN
+ )
+ 
+SET HasAdminRights=0
+
+FOR /F %%i IN ('WHOAMI /PRIV /NH') DO (
+	IF "%%i"=="SeTakeOwnershipPrivilege" SET HasAdminRights=1
+)
+
+IF NOT %HasAdminRights%==1 (
+	ECHO .
+	ECHO Not enough permissions to run the script !!!
+	ECHO .
+	GOTO END
+)
+
+:SKIPADMIN
+
+rem Получаем версию ОС
+rem Windows XP ?
+
 ver | find "5.1."
 
 If %errorlevel%==0  (
 	rem Windows XP
-	SCHTASKS /Create /RU "NT AUTHORITY\SYSTEM" /SC HOURLY /ST 09:00:00 /TN "AV_Common_Status_00" /TR "\"C:\zabbix\scripts\AV_Common_Status\AV_Common_Status.bat\""
-	SCHTASKS /Create /RU "NT AUTHORITY\SYSTEM" /SC HOURLY /ST 09:15:00 /TN "AV_Common_Status_15" /TR "\"C:\zabbix\scripts\AV_Common_Status\AV_Common_Status.bat\""
-	SCHTASKS /Create /RU "NT AUTHORITY\SYSTEM" /SC HOURLY /ST 09:30:00 /TN "AV_Common_Status_30" /TR "\"C:\zabbix\scripts\AV_Common_Status\AV_Common_Status.bat\""
-	SCHTASKS /Create /RU "NT AUTHORITY\SYSTEM" /SC HOURLY /ST 09:45:00 /TN "AV_Common_Status_45" /TR "\"C:\zabbix\scripts\AV_Common_Status\AV_Common_Status.bat\""
+	SCHTASKS /Create /RU "NT AUTHORITY\SYSTEM" /SC HOURLY /ST 00:00:00 /TN "AV_Common_Status_00" /TR "\"C:\zabbix\scripts\AV_Common_Status\AV_Common_Status.bat\""
+	SCHTASKS /Create /RU "NT AUTHORITY\SYSTEM" /SC HOURLY /ST 00:15:00 /TN "AV_Common_Status_15" /TR "\"C:\zabbix\scripts\AV_Common_Status\AV_Common_Status.bat\""
+	SCHTASKS /Create /RU "NT AUTHORITY\SYSTEM" /SC HOURLY /ST 00:30:00 /TN "AV_Common_Status_30" /TR "\"C:\zabbix\scripts\AV_Common_Status\AV_Common_Status.bat\""
+	SCHTASKS /Create /RU "NT AUTHORITY\SYSTEM" /SC HOURLY /ST 00:45:00 /TN "AV_Common_Status_45" /TR "\"C:\zabbix\scripts\AV_Common_Status\AV_Common_Status.bat\""
  ) else (
 	rem НЕ Windows XP
-	SCHTASKS /Create /RU "NT AUTHORITY\SYSTEM" /SC DAILY /ST 09:00 /RI 10 /DU 12:00 /TN "AV_Common_Status" /TR "\"C:\zabbix\scripts\AV_Common_Status\AV_Common_Status.bat\"" /RL HIGHEST /F
+	SCHTASKS /Create /RU "NT AUTHORITY\SYSTEM" /SC DAILY /ST 00:00 /RI 15 /DU 24:00 /TN "AV_Common_Status" /TR "\"C:\zabbix\scripts\AV_Common_Status\AV_Common_Status.bat\"" /RL HIGHEST /F
  )
+
+CALL C:\zabbix\scripts\AV_Common_Status\AV_Common_Status.bat
+
+:END
 
 EXIT /B
