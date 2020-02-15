91443c85acfa5fd6522d6b0cd1f87d5a2e690afc
mihanik <mihanik2000@mail.ru>
2020-02-15 19:59:54 +0300

    / ‘zabbix/scripts/WinSmartInfo/SMART.py’

--- zabbix/scripts/WinSmartInfo/SMART.py
+++ zabbix/scripts/WinSmartInfo/SMART.py
@@ -64,8 +64,8 @@ def main(argv=None):
 
    for MyLine in MyFullAtaList:
       print MyLine
-
-    get_sdx_list (MyFullAtaList)
+      
+   MySDXList = get_sdx_list(MyFullAtaList)
 
 if __name__ == "__main__":
     sys.exit(main())
