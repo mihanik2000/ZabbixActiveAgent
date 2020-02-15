e9f30ae2f4117385706d30b254c5612b7dafb120
mihanik <mihanik2000@mail.ru>
2020-02-15 19:41:20 +0300

    / ‘zabbix/scripts/WinSmartInfo/SMART.py’

--- zabbix/scripts/WinSmartInfo/SMART.py
+++ zabbix/scripts/WinSmartInfo/SMART.py
@@ -15,6 +15,20 @@ def is_admin():
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
+      
+#
+# Функция получения STDOUT команды DOS
+# Вход: командная строка
+# Выход: список строк из STDOUT
+#
+def get_stdout( MyCmdLine )
+   MySTDOUT=[]
+   try:
+      MySTDOUT = os.popen(MyCmdLine).read().splitlines()
+   except Exception:
+      pass
+   finally:
+      return MySTDOUT
 
 ################################################################################
 #   Начало программы
@@ -29,56 +43,8 @@ def main(argv=None):
    reload(sys)
    sys.setdefaultencoding('utf8')
    
-   # Устанавливаем имена файлов, где будет храниться температура процесcоров
-   cpu0 = 'C:/zabbix/scripts/OHMR/cpu0.txt'
-   cpu1 = 'C:/zabbix/scripts/OHMR/cpu1.txt'
-   
-   # Получаем результат работы OHMR
-   OHMR = []
-   OHMR = os.popen("C:\zabbix\scripts\OHMR\OpenHardwareMonitorReport.exe").read().splitlines()
-   
-   # Высчитываем температуру 0-го и, возможно, 1-го процесора
-   
-   n0 = 0
-   sum0 = 0
-   
-   n1 = 0
-   sum1 = 0
-   
-   for i in OHMR:
-      if (':' in i) and ('(/intelcpu/0/temperature/' in i):
-         t = i.split()
-         n0 = n0 + 1
-         sum0 = sum0 + int(t[8])
-   
-      if (':' in i) and ('(/intelcpu/1/temperature/' in i):
-         t = i.split()
-         n1 = n1 + 1
-         sum1 = sum1 + int(t[8])
-   
-   if n0>0:
-      sum0 = sum0 / n0
-   
-   if n1>0:
-      sum1 = sum1 / n1
-   
-   # Сохраняем температуру 0-го процессора в файл
-   f = open(cpu0,'w')
-   try:
-      f.write(str(sum0))
-   except Exception:
-      pass
-   finally:
-      f.close()
-   
-   # Сохраняем температуру 1-го процессора в файл
-   f = open(cpu1,'w')
-   try:
-      f.write(str(sum1))
-   except Exception:
-      pass
-   finally:
-      f.close()
+   MyList = get_stdout ('smartctl.exe --scan')
+   print MyList
 
 if __name__ == "__main__":
     sys.exit(main())
