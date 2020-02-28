850e03bb9fe5ce2e4d1c130780a9538eae97229f
mihanik <mihanik2000@mail.ru>
2020-02-28 22:28:20 +0300

    / ‘zabbix/scripts/Updater/updater.py’

---
+++ zabbix/scripts/Updater/updater.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+import ctypes
+import os
+
+#
+# Функция проверки наличия прав администратора
+# Вход: нет
+# Выход: true - есть права администратора,
+#        false - нет прав администратора
+#
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+    
+# 
+# Функция загрузки списка из файла
+# Вход: путь к файлу со списком
+# Выход: список
+#
+def FileToList ( InboundFileName ):
+    OutboundList = []
+    
+    # Если указанного файла не существует возвращаем пустой список
+    if not os.path.exists( InboundFileName ):
+        return OutboundList
+    
+    if not os.path.isfile( InboundFileName ):
+        return OutboundList
+    
+    # Читаем файл построчно
+    f = open( InboundFileName,'r')
+    try:
+        OutboundList = f.read().splitlines()
+    except Exception:
+        pass
+    finally:
+        f.close()
+
+    return OutboundList
+
+# 
+# Функция сохранения списка в файл
+# Вход: список, путь к файлу
+# Выход: true - успешная запись, false - ошибка при записи
+#
+def ListToFile ( OutboundList, OutboundFileName ):
+    
+    # Записываем файл построчно
+   f = open( OutboundFileName,'w')
+   try:
+     for line in OutboundList:
+           f.write(line + '\n')
+   except Exception:
+       MyResult = False
+   else:
+       MyResult = True
+   finally:
+      f.close()
+    
+   return MyResult
