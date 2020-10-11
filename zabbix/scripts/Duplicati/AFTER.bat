9e74c619ce90baed19334d61b7eba595217daca7
mihanik <mihanik2000@mail.ru>
2020-10-11 05:25:19 +0300

    + ‘zabbix/scripts/Duplicati/’ + ‘zabbix/scripts/Duplicati/AFTER.bat’ + ‘zabbix/scripts/Duplicati/BEFORE.bat’

---
+++ zabbix/scripts/Duplicati/AFTER.bat
@@ -0,0 +1,4 @@
+echo %DUPLICATI__EVENTNAME% >> C:\zabbix\scripts\DUPLICATI\DUPLICATI.txt
+echo BackUp finished at: %date% %time% >> C:\zabbix\scripts\DUPLICATI\DUPLICATI.txt
+echo BackUp result: %DUPLICATI__PARSED_RESULT% >> C:\zabbix\scripts\DUPLICATI\DUPLICATI.txt
+
---
+++ zabbix/scripts/Duplicati/BEFORE.bat
@@ -0,0 +1,2 @@
+echo %DUPLICATI__EVENTNAME% > C:\zabbix\scripts\DUPLICATI\DUPLICATI.txt
+echo BackUp started at: %date% %time% >> C:\zabbix\scripts\DUPLICATI\DUPLICATI.txt
