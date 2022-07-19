5783fc2738d20cb16ea7fbbf69341cb680e4e94c
Local <mihanik2000@mail.ru>
2022-07-19 11:34:17 +0300

    Немного прибрался в файлах
    Немного прибрался в файлах

--- zabbix/scripts/WinSmartInfo/WinSmartInfo.bat
+++ zabbix/scripts/WinSmartInfo/WinSmartInfo.bat
@@ -1,7 +1,7 @@
 @echo off
 
-rem "C:\Program Files\smartmontools\bin\smartctl.exe" -s on -T permissive -d sat --all %systemdrive% > C:\zabbix\scripts\WinSmartInfo\WinSmartInfo.txt
+rem "C:\Program Files\smartmontools\bin\smartctl.exe" -s on -T permissive -d sat --all %systemdrive% > C:\zabbix\scripts\WinSmartInfo\WinSmartInfo_sda.txt
 
-"C:\Program Files\smartmontools\bin\smartctl.exe" -s on -T permissive --all /dev/sda > C:\zabbix\scripts\WinSmartInfo\WinSmartInfo.txt
+"C:\Program Files\smartmontools\bin\smartctl.exe" -s on -T permissive --all /dev/sda > C:\zabbix\scripts\WinSmartInfo\WinSmartInfo_sda.txt
 
 exit /b
